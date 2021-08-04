d31d439796280b25af2e660f73039101c9f32cca
zakigaebal <zakigaebal@gmail.com>
2021-08-04 22:17:58 +0900

    wqdqwd

---
+++ js/share.js
@@ -0,0 +1,71 @@
+const url = 'https://www.namoham.com/pigtest';
+const title = '당신은 돼지인가?ㅋㅋㅋ ';
+const hash = '%2310_things_test %233분이미지테스트 %23이미지테스트 ';
+
+
+function setShare(){
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '당신의 결과는???';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + '/img/image-' + resultAlt + '.png';
+  const shareURL = url + '/page/result-' + resultAlt + '.html';
+
+  Kakao.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDes,
+      imageUrl: shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+        webUrl: shareURL
+      },
+    },
+
+    buttons: [
+      {
+        title: '결과확인하기',
+        link: {
+          mobileWebUrl: shareURL,
+          webUrl: shareURL,
+        },
+      },
+    ]
+  });
+}
+
+const copy = () => {
+  const tmp = document.createElement('textarea');
+  document.body.appendChild(tmp);
+  tmp.value = url;
+  tmp.select();
+  document.execCommand('copy');
+  document.body.removeChild(tmp);
+}
+
+
+const fb = () => {
+  window.open('https://www.facebook.com/sharer/sharer.php?u='
+    + url + '&t=' + title + '' + hash,
+    'facebooksharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+  return false;
+}
+const tw = () => {
+  window.open('https://twitter.com/intent/tweet?text='
+    + title + '%0A' + '저는 ' + '' + hash + '%0A' + url,
+    'twittersharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
+  return false;
+}
+const nv = () => {
+  window.open('http://share.naver.com/web/shareView.nhn?url='
+    + url + '&title=' + title,
+    'naversharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+  return false;
+}
+const band = () => {
+  window.open('https://band.us/plugin/share?url='
+    + url + '&title=' + title,
+    'naversharedialog', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+  return false;
+}
